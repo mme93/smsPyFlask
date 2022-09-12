8a36bc6bea484054d8c1a84ce0e9503124b807fa
magantus@googlemail.com <Extrem93!>
2022-09-12 16:14:15 +0200

    update

--- GSM.py
+++ GSM.py
@@ -1,85 +1,26 @@
-import sys
 import time
 
 import serial
-from serial import Serial
 
 
-def isReady(ser):
+# Create a Serial Port for GSM-Module
+def createSerial():
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.timeout = 5
+    ser.port = "/dev/ttyS0"
+    return ser
+
+
+def openSerialPort(ser):
     if not ser.is_open:
         ser.open()
         print("Status: ", ser.is_open)
     return ser.is_open
 
-def sendPin(ser):
+
+# Check AT Status from GSM-Module
+def checkATStatus(ser):
     ser.write(b'AT\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-    """
-    ser.write(b'AT+CPIN?\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-    """
-    ser.write(b'AT+CPIN=5511\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-
-    ser.write(b'AT+CREG?\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-
-    ser.write(b'AT+CMGF=1\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-
-    ser.write(b'AT+CMGS="+4917684582550"\r')
-    #ser.write(b'AT+CMGS="+4915734696774"\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-
-    ser.write(b'Ich bin ein Text\r')
-    # ser.write(b'AT+CMGS="+4915734696774"\r')
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-
-    ser.write(chr(26))
-    time.sleep(1)
-    myline = ser.readline()
-    while myline:
-        print(myline)
-        myline = ser.readline()
-    #+4915734696774
-
-
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.timeout = 5
-ser.port = "/dev/ttyS0"
-
-if not isReady(ser):
-    print("Serial is not open")
-    sys.exit(0)
-
-print("Serial is open")
-sendPin(ser)
+    time.sleep(2)
+    return ser.readline()
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import serial
+import GSM
 
 app = Flask(__name__)
 
@@ -6,6 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/isReady')
+def isSIMReady():
+    ser = GSM.createSerial()
+    if GSM.openSerialPort(ser):
+        print(ser.is_open)
+        return 'Is Ready'
+    else:
+        return 'Can´t open Serial Port'
 
 
 if __name__ == '__main__':
